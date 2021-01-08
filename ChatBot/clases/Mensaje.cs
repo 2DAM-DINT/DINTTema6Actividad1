c8f379e6ab40602e26b9239ab7603367d123c89f
alumnot1 <alumnot1@ORD12>
2021-01-08 11:51:13 +0100

    Agregar archivos de proyecto.

---
+++ ChatBot/clases/Mensaje.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+
+namespace ChatBot.clases
+{
+    public class Mensaje : INotifyPropertyChanged
+    {
+        public enum Emisor { Usuario, Robot }
+
+        private Emisor remitente;
+        public Emisor Remitente
+        {
+            get { return this.remitente; }
+            set
+            {
+                if (this.remitente != value)
+                {
+                    this.remitente = value;
+                    this.NotifyPropertyChanged("Remitente");
+                }
+            }
+        }
+
+        private string texto;
+        public string Texto
+        {
+            get { return this.texto; }
+            set
+            {
+                if (this.texto != value)
+                {
+                    this.texto = value;
+                    this.NotifyPropertyChanged("Texto");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Mensaje(string emisor, string texto)
+        {
+            Emisor = emisor;
+            Texto = texto;
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
